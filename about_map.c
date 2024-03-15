08c02c1d1e401eab066b36b44eb784a658ef2ebe
thehanbear <gratefuldays34@gmail.com>
2024-03-15 11:51:24 +0200

    added

--- about_map.c
+++ about_map.c
@@ -6,49 +6,79 @@
 /*   By: sunmac <sunmac@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/13 09:55:07 by hlee-sun          #+#    #+#             */
-/*   Updated: 2024/03/14 00:12:26 by sunmac           ###   ########.fr       */
+/*   Updated: 2024/03/15 11:50:18 by sunmac           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
+// // # ifndef SO_LONG_H
+// // # define SO_LONG_H
+
+// # include <stdlib.h>
+// # include <fcntl.h>
+// # include <unistd.h>
+// # include "./Libft/libft.h" 
+// // # include "./MLX42/MLX42.h"
+
+// # include <stdio.h>
+
+// typedef struct	s_game
+// {
+// 	char 	**map;
+// 	int		width;
+// 	int		height;
+// 	int		move;
+// 	char	*first_line;
+// 	char	*lines;
+// }	t_game;
+
 int	verify_map_name(const char *file_name, const char *ending)
 {
- 	int	i;
- 	int len;
- 	int	j;
-	
- 	i = 0;
- 	len = ft_strlen(file_name);
- 	j = len - ft_strlen(ending);
- 	while (j < len)
- 	{
- 		if (file_name[j] != ending[i])
- 			return (1);
- 		i++;
-		j++;
- 	}
- 	return 0;
+	int	i;
+	int	name_len;
+	int	ending_len;
+	int	len;
+
+	i = 0;
+	name_len = ft_strlen(file_name);
+	ending_len = ft_strlen(ending);
+	if (ending_len > name_len)
+		return (1);
+	len = name_len - ending_len;
+	while (file_name[len] != '\0' && ending[i] != '\0')
+	{
+		if (file_name[len] != ending[i])
+			return (1);
+		i++;
+		len++;
+	}
+	return (0);
 }
 
-char	*read_map(char *map)
+void	*read_map(char *map, t_game *game)
 {
-	int	fd;
-	int	size;
-	int	*line;
+	int		fd;
+	char	*line;
 
-	if (verify_map_name(map, ".ber") != 0)
-		print_error("Map file name error");
 	fd = open(map, O_RDONLY);
-
-
-	if (fd < 0 || read (fd, 0, 0) < 0)
-		return (ft_free(&line));
-	map = check_map(map);
-	if (!map)
-		return (0);
-	line = (char)malloc * (sizeof(map) + 1);
-	if (!line)
-		return (ft_free(&line))
-	
+	if (fd <= 0)
+		return (NULL);
+	line = get_next_line (fd);
+	game->height = 0;
+	game->width = ft_strlen(line) - 1;
+	game->lines = ft_strdup(line);
+	free(line);
+	while (line != 0)
+	{
+		game->height++;
+		line = get_next_line (fd);
+		if (line != 0)
+		{
+			game->lines = ft_strjoin(game->lines, line);
+		}
+	}
+	close(fd);
+	printf("%s\n", game->lines);
+	return (NULL);
 }
