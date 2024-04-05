08e3a09ab3053a5f72339e56ffc8d1c52b005650
Han_B <150435543+thehanbear@users.noreply.github.com>
2024-04-05 11:55:56 +0300

    Update main.c

--- main.c
+++ main.c
@@ -5,27 +5,28 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: hlee-sun <hlee-sun@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/15 15:13:14 by hlee-sun          #+#    #+#             */
-/*   Updated: 2024/03/19 15:05:45 by hlee-sun         ###   ########.fr       */
+/*   Created: 2024/04/02 14:28:11 by hlee-sun          #+#    #+#             */
+/*   Updated: 2024/04/03 17:41:39 by hlee-sun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-    t_game  game;
-    int     fd;
-    
-    if (argc != 2)
-        print_error(1);
-    init_param(&game);
-    verify_map_name(argv[1], ".ber");
-    fd = open(argv[1], O_RDONLY);
-    read_map(fd, &game);
-    close (fd);
-    validate_map(&game);
-    free_game(&game);
-    return (0);
-}
+	t_game	*game;
+	int		fd;
 
+	game = malloc(sizeof(t_game));
+	if (argc != 2)
+		print_error(1);
+	init_param(game);
+	verify_map_name(argv[1]);
+	fd = open(argv[1], O_RDONLY);
+	read_map(fd, game);
+	close (fd);
+	validate_map(game);
+	about_mlx(game);
+	free_game(game);
+	return (0);
+}
